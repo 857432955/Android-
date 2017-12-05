f4ca83927515dbab8c915290cc0c476d1fd86f6a
zrx <857432955@qq.com>
2017-12-05 09:48:21 +0800

    防止按钮多次点击工具类

---
+++ java-util工具类/utils工具类/ClickUtils.java
@@ -0,0 +1,22 @@
+package com.zhang.jnhj.utils;
+
+/**
+ * 防止按钮多次点击
+ * Created by ZhangRuxing.
+ */
+
+public class ClickUtils {
+    // 两次点击按钮之间的点击间隔不能少于1000毫秒
+    private static final int MIN_CLICK_DELAY_TIME = 1000;
+    private static long lastClickTime;
+
+    public static boolean isFastClick() {
+        boolean flag = false;
+        long curClickTime = System.currentTimeMillis();
+        if ((curClickTime - lastClickTime) >= MIN_CLICK_DELAY_TIME) {
+            flag = true;
+        }
+        lastClickTime = curClickTime;
+        return flag;
+    }
+}
